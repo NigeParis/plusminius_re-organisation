77d3b7456dbb1c6a9a989bed1389da7291f61199
nigel robinson <nigelrobinson@MacBook.local>
2023-05-24 12:16:27 +0200

    passed norminette

--- ft_interface.h
+++ ft_interface.h
@@ -6,17 +6,17 @@
 /*   By: nigelrobinson <Nige@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:10:56 by nigelrobinson     #+#    #+#             */
-/*   Updated: 2023/05/24 12:12:33 by nigelrobinson    ###   ########.fr       */
+/*   Updated: 2023/05/24 12:15:52 by nigelrobinson    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_INTERFACE_H
 # define FT_INTERFACE_H
 
-void    ft_print_header(void);
-void    ft_clear_screen(void);
-void    ft_print_game_over(int score);
-void    ft_prompt_number_guess(int level);
-void    ft_print_signoff(void);
-int     ft_get_difficulty_setting(void);
+void	ft_print_header(void);
+void	ft_clear_screen(void);
+void	ft_print_game_over(int score);
+void	ft_prompt_number_guess(int level);
+void	ft_print_signoff(void);
+int		ft_get_difficulty_setting(void);
 #endif
