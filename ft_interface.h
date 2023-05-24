b6e6f5eb54c32a5f5c22434ea8ee0e0a3c7f441e
nigel robinson <nigelrobinson@MacBook.local>
2023-05-24 12:13:50 +0200

    clean header interface.h

--- ft_interface.h
+++ ft_interface.h
@@ -1,4 +1,17 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_interface.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nigelrobinson <Nige@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 12:10:56 by nigelrobinson     #+#    #+#             */
+/*   Updated: 2023/05/24 12:12:33 by nigelrobinson    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_INTERFACE_H
+# define FT_INTERFACE_H
 
 void    ft_print_header(void);
 void    ft_clear_screen(void);
@@ -6,3 +19,4 @@ void    ft_print_game_over(int score);
 void    ft_prompt_number_guess(int level);
 void    ft_print_signoff(void);
 int     ft_get_difficulty_setting(void);
+#endif
